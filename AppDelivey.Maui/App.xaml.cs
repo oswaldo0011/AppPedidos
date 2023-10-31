0844f4a10d045c14689cf73c32d4721d239bbb03
oswaldo ocotan <oswaldosanchez2211@gmail.com>
2023-10-30 20:52:30 -0600

    otro repo

---
+++ AppDelivey.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppDelivey.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ AppDelivey.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppDelivey.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
